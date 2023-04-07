75ac595e7b664c989a785bdf47c316c0b3144c6a
Benito Ernst <benito.ernst@t-online.de>
2023-04-07 10:03:07 +0200

    finished

--- Aufgabenteil d)/contact_form.php
+++ Aufgabenteil d)/contact_form.php
@@ -3,15 +3,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Input</title>
+    <title>Projektkontakt</title>
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
 
     <nav>
     <?php echo "
-            <a href='index.php'>Index</a> 
-            <a href='contact_form.php'>Form</a> 
-            <a href='num_form.php'>Num</a> 
+           <a href='index.php'>Hauptseite</a> 
+           <a href='contact_form.php'>Projektkontakt</a> 
+           <a href='num_form.php'>Zahlauswahl</a> 
         ";?>
     </nav>
 
@@ -20,7 +21,11 @@
 
     <?php 
 
-    $input = $_GET['number']; 
+    if(isset($_GET['number'])){
+
+        $input = $_GET['number'];
+
+    }
 
 
     class Contact{
@@ -43,20 +48,21 @@
     $contact4 = new Contact("Max4", "Mustermann4", "test4@mail.com", "444444444");
     $contact5 = new Contact("Max5", "Mustermann5", "test5@mail.com", "555555555");
 
-    $contacts = [$contact1, $contact2, $contact3, $contact4, $contact5];
+    $contacts = [1 => $contact1, 2 => $contact2, 3 => $contact3, 4 => $contact4, 5 => $contact5];
 
     ?>
 
+    <h1>Projektkontakt</h1>
     
-    <form action="" method="post">
-        <label for="fname">Vorname:</label>
-        <input type="text" id="fname" name="vorname" value="<?php echo $contacts[$input]->vorname; ?>"><br><br>
+    <form action="num_form.php" method="get">
         <label for="lname">Nachname:</label>
-        <input type="text" id="lname" name="nachname" value="<?php echo $contacts[$input]->nachname; ?>"><br><br>
+        <input type="text" id="lname" name="nachname" placeholder="Texteingabe" value="<?php echo (isset($input)) ? $contacts[$input]->nachname : false; ?>"><br><br>
+        <label for="fname">Vorname:</label>
+        <input type="text" id="fname" name="vorname" placeholder="Texteingabe" value="<?php echo (isset($input)) ? $contacts[$input]->vorname : false; ?>"><br><br>
         <label for="email">Email:</label>
-        <input type="email" id="email" name="email" value="<?php echo $contacts[$input]->email; ?>"><br><br>
+        <input type="email" id="email" name="email" placeholder="Eingabe einer E-Mailadresse" value="<?php echo (isset($input)) ? $contacts[$input]->email : false; ?>"><br><br>
         <label for="tel">Telefonnummer:</label>
-        <input type="tel" id="tel" name="telefon" value="<?php echo $contacts[$input]->telefon; ?>"><br><br>
+        <input type="tel" id="tel" name="telefon" placeholder="Eingabe v. Ortsvorwahl und Anschluss" value="<?php echo (isset($input)) ? $contacts[$input]->telefon : false; ?>"><br><br>
         <input type="submit" value="Submit">
     </form>
 
@@ -67,6 +73,7 @@
     <footer>
         copyright Benito Ernst
     </footer>
+
     
 </body>
 </html>
